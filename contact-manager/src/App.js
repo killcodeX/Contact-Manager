5743a805b6671cd3523f73d554ffb341981269d9
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-03 08:09:52 +0530

    working with props

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-      <Contact/>
-      <Contact/>
-      <Contact/>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Header branding="Contact Manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+        <Contact name="Farhana" email="farhanax95@gmail.com" phone="333-333-3333"/>
+      </div>
+    );
+  }
 }
 
 export default App;
