4752665f52969afbe58abe270ece95423eb69ede
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-03 07:23:32 +0530

    created state component

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi am React App</h1>
+      <h1>The App Component</h1>
+      <Contact/>
+      <Contact/>
+      <Contact/>
     </div>
   );
 }
