01c52d222c8de92ba182e34f200565b04316b176
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-07 05:21:13 +0530

    added container

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -10,8 +10,10 @@ class App extends Component{
     return (
       <div className="App">
         <Header branding="Contact Manager"/>
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
-        <Contact name="Farhana" email="farhanax95@gmail.com" phone="333-333-3333"/>
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+          <Contact name="Farhana" email="farhanax95@gmail.com" phone="333-333-3333"/>
+        </div>
       </div>
     );
   }
