5c8ff0075ee9f02f3fa41f251f0093ad9ce21bd3
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-10 04:08:22 +0530

    added sort down icon

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
     render() {
         const {name, email, phone} = this.props.contact;
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <i class="fas fa-sort-down"></i></h4>
+                <h4>{name} <FontAwesomeIcon icon={faSortDown} /></h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
