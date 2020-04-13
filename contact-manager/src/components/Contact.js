18e8df1df2b85c197445cb3be2bbd6fa48c50999
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-13 15:00:42 +0530

    added event listeners

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -4,11 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
+    state = {};
+    onShowClick = (e) =>{
+        
+    }
+
     render() {
         const {name, email, phone} = this.props.contact;
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <FontAwesomeIcon icon={faSortDown} /></h4>
+                <h4>{name} <FontAwesomeIcon icon={faSortDown} onClick={this.onShowClick}/></h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
