55d2b2387145ae9bf03aabbe7c514e7316668545
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-13 22:11:40 +0530

    used setstate

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -4,20 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
-    state = {};
+    state = {
+        showContactInfo: false
+    };
+
     onShowClick = (e) =>{
-        
+        this.setState({showContactInfo : !this.state.showContactInfo });
     }
 
     render() {
         const {name, email, phone} = this.props.contact;
+        const { showContactInfo } = this.state;
+
         return (
             <div className="card card-body mb-3">
                 <h4>{name} <FontAwesomeIcon icon={faSortDown} onClick={this.onShowClick}/></h4>
-                <ul className="list-group">
+                {showContactInfo ? ( <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                </ul>) : null}
+                
             </div>
         )
     }
