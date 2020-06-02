da598cc3d084ae21248bcd5b2eddefb55b903b9b
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-06-02 19:21:45 +0530

    added form validation

--- contact-manager/src/components/layout/textInputGroup.js
+++ contact-manager/src/components/layout/textInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 function TextInputGroup({
     label,
@@ -7,12 +8,14 @@ function TextInputGroup({
     value,
     placeholder,
     type,
-    onChange
+    onChange,
+    error
 }) {
     return (
         <div className='form-group'>
             <label htmlFor={name}>{label}</label>
-            <input type={type}  name={name} className='form-control form-control-lg' placeholder={placeholder} value={value} onChange={onChange}/>
+            <input type={type}  name={name} className={classnames('form-control form-control-lg',{'is-invalid':error})} placeholder={placeholder} value={value} onChange={onChange}/>
+            {error && <div className="invalid-feedback">{error}</div>}
         </div>
     )
 }
@@ -23,7 +26,8 @@ TextInputGroup.prototype ={
     placeholder: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    error : PropTypes.string.isRequired
 }
 
 TextInputGroup.defaultProps = {
--- contact-manager/src/contacts/addContact.js
+++ contact-manager/src/contacts/addContact.js
@@ -8,12 +8,31 @@ class AddContact extends Component {
     state = {
         name : '',
         email : '',
-        phone : ''
+        phone : '',
+        errors : {}
     }
 
     Submit = (dispatch,e) =>{
         e.preventDefault();
-        const {name, email, phone} = this.state;
+        const {name, email, phone, errors} = this.state;
+
+        // check for Errors
+        if(name === ''){
+            this.setState({errors: { name: 'Name is required'}})
+            return;
+        }
+
+        if(email === ''){
+            this.setState({errors: { email: 'Email is required'}})
+            return;
+        }
+
+        if(phone === ''){
+            this.setState({errors: { phone: 'Phone is required'}})
+            return;
+        }
+
+
         const newContact = {
             id: uuid(),
             name,
@@ -27,7 +46,8 @@ class AddContact extends Component {
         this.setState({
             name : '',
             email : '',
-            phone : ''
+            phone : '',
+            errors : {}
         })
     }
 
@@ -37,7 +57,7 @@ class AddContact extends Component {
 
     render() {
 
-        const {name, email, phone} = this.state;
+        const {name, email, phone, errors} = this.state;
 
         return(
             <Consumer>
@@ -48,9 +68,9 @@ class AddContact extends Component {
                             <div className='card-header'>Add Contact</div>
                             <div className='card-body'>
                                 <form onSubmit={this.Submit.bind(this, dispatch)}>
-                                    <TextInputGroup label='Name' name='name' placeholder='Enter your name..' value={name} onChange={this.stateChange}/>
-                                    <TextInputGroup label='Email' name='email' type='email' placeholder='Enter your email..' value={email} onChange={this.stateChange}/>
-                                    <TextInputGroup label='Phone' name='phone' placeholder='Enter your phone no..' value={phone} onChange={this.stateChange}/>
+                                    <TextInputGroup label='Name' name='name' placeholder='Enter your name..' value={name} onChange={this.stateChange} error={errors.name}/>
+                                    <TextInputGroup label='Email' name='email' type='email' placeholder='Enter your email..' value={email} onChange={this.stateChange} error={errors.email}/>
+                                    <TextInputGroup label='Phone' name='phone' placeholder='Enter your phone no..' value={phone} onChange={this.stateChange} error={errors.phone}/>
                                     <input type='submit' value='Add contact' className='btn btn-block btn-light' />
                                 </form>
                             </div>
