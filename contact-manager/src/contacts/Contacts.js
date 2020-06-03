8ab9ce9e85acdf05f0a2a3ea0bb07ad199ae9020
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-06-03 11:00:02 +0530

    setup the route

--- contact-manager/src/contacts/Contacts.js
+++ contact-manager/src/contacts/Contacts.js
@@ -12,6 +12,7 @@ class Contacts extends Component {
                     const { contacts } = value;
                     return(
                         <React.Fragment>
+                            <h1 className='display-4 mb-3'><span className='text-danger'>Contact</span> List</h1>
                             {contacts.map(contact => (
                                 <Contact key={contact.id}  contact={contact}/>
                             ))}
